b431766653719587a18bb929ce85cd58ad3cb3f2
Uladzislau Volchyk <uladzislau.volchyk@gmail.com>
2020-11-05 16:24:27 +0300

    feat: add dialog for empty field

--- lab2/ActivityIntent/app/src/main/java/com/example/activityintent/MainActivity.kt
+++ lab2/ActivityIntent/app/src/main/java/com/example/activityintent/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.activityintent
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     companion object Constant {
-        val EXTRA_KEY = "com.example.activityintent"
-        val REQUEST_CODE = 80
+        const val EXTRA_KEY = "com.example.activityintent"
+        const val REQUEST_CODE = 80
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,16 +26,29 @@ class MainActivity : AppCompatActivity() {
             text = 0.toString()
         }
 
-        val confirmButton = findViewById<Button>(R.id.button)
-        confirmButton.setOnClickListener {
-            val newScoreView = findViewById<TextView>(R.id.scoreInputView)
-            val newValue = newScoreView.text.toString().toInt()
-            val initialValue = initialScoreView.text.toString().toInt()
+        findViewById<Button>(R.id.button).setOnClickListener {
 
-            val intent = Intent(this, SlaveActivity::class.java).apply {
-                putExtra(EXTRA_KEY, newValue + initialValue)
+            val newScoreView = findViewById<TextView>(R.id.scoreInputView)
+            val newValue = newScoreView.text.toString()
+
+            if (newValue == "") {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(R.string.warning_title)
+                builder.setMessage(R.string.warning_message)
+                builder.setIcon(android.R.drawable.ic_dialog_alert)
+                builder.setPositiveButton(R.string.warning_neutral){ _, _ -> }
+
+                val alertDialog: AlertDialog = builder.create()
+                alertDialog.setCancelable(false)
+                alertDialog.show()
+            } else {
+                val initialValue = initialScoreView.text.toString().toInt()
+
+                val intent = Intent(this, SlaveActivity::class.java).apply {
+                    putExtra(EXTRA_KEY, newValue.toInt() + initialValue)
+                }
+                startActivityForResult(intent, REQUEST_CODE)
             }
-            startActivityForResult(intent, REQUEST_CODE)
         }
     }
 
--- lab2/ActivityIntent/app/src/main/java/com/example/activityintent/SlaveActivity.kt
+++ lab2/ActivityIntent/app/src/main/java/com/example/activityintent/SlaveActivity.kt
@@ -1,6 +1,7 @@
 package com.example.activityintent
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -18,14 +19,26 @@ class SlaveActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.button).setOnClickListener {
             val newScoreView = findViewById<TextView>(R.id.scoreInputView)
-            val newValue = newScoreView.text.toString().toInt()
+            val newValue = newScoreView.text.toString()
             val initialValue = initialScoreView.text.toString().toInt()
 
-            val returnIntent = Intent().apply {
-                putExtra(MainActivity.EXTRA_KEY, newValue + initialValue)
+            if (newValue == "") {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(R.string.warning_title)
+                builder.setMessage(R.string.warning_message)
+                builder.setIcon(android.R.drawable.ic_dialog_alert)
+                builder.setPositiveButton(R.string.warning_neutral){ _, _ -> }
+
+                val alertDialog: AlertDialog = builder.create()
+                alertDialog.setCancelable(false)
+                alertDialog.show()
+            } else {
+                val returnIntent = Intent().apply {
+                    putExtra(MainActivity.EXTRA_KEY, newValue.toInt() + initialValue)
+                }
+                setResult(Activity.RESULT_OK, returnIntent)
+                finish()
             }
-            setResult(Activity.RESULT_OK, returnIntent)
-            finish()
         }
     }
 }
